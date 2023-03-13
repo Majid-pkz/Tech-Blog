24e72b2809d7645c26bcd5d52b335a3c0886b143
Majid-pkz <majid.computing@gmail.com>
2023-03-13 17:14:55 +1100

    create new post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,5 +3,16 @@ const router = require('express').Router();
 
 
 
+router.post('/', async (req, res) => {
+    try {
+        const postData = await Post.create(req.body);
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+
 
 module.exports = router;
--- public/js/newpost.js
+++ public/js/newpost.js
@@ -1,48 +1,36 @@
-// function display(){
-//  console.log("add new post")
-//  console.log(input_title)
-//  console.log(input_content)
-// }
+
+const newPostHandler = async () => {
+
+    // Get the value of the "Title" input
+    var title = document.getElementById("myTitle").value;
+  
+    // Get the value of the "Content" input
+    var content = document.getElementById("myContent").value;
+  
+    
+    if (title&& content) {
+       const response = await fetch('/api/posts', {
+          method: 'POST',
+          body: JSON.stringify({ title, content }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (response.ok) {
+          console.log(title);
+          console.log(content);
+          document.location.replace('/');
+        } else {
+          alert('Failed to sign up.');       }
+   
+    
+  }
+};
+  
+  
  
-// const input_title = document.querySelector('#form-title').value.trim();
-// const input_content = document.querySelector('#form-content').value.trim();
 
-
-
-
-
-// document.querySelector('#create').addEventListener('click',display)
-
-
-
-const newPostHandler = async (event) => {
-    event.preventDefault();
   
-    const input_title = document.querySelector('#form-title').value.trim();
-    const input_content = document.querySelector('#form-content').value.trim();
-  
-    if (input_title&& input_content) {
-    //   const response = await fetch('/api/users', {
-    //     method: 'POST',
-    //     body: JSON.stringify({ username, email, password }),
-    //     headers: { 'Content-Type': 'application/json' },
-    //   });
-  
-    //   if (response.ok) {
-    //     document.location.replace('/');
-    //   } else {
-    //     alert('Failed to sign up.');
-    //   }
-    console.log("add new post")
-    console.log(input_title)
-    console.log(input_content)
 
 
-    }
-  };
 
-  document.querySelector('#create').addEventListener('click',newPostHandler)
-
-// document
-//   .querySelector('.signup-form')
-//   .addEventListener('submit', signupFormHandler);
+document.getElementById("create").addEventListener("click", newPostHandler)
