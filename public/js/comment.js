37b838d94c7f938eb2e2d758700e22b2b50dd1f8
Majid-pkz <majid.computing@gmail.com>
2023-03-15 00:37:57 +1100

    add coment.js frontend

---
+++ public/js/comment.js
@@ -0,0 +1,28 @@
+// replace with newComsHandler
+const newPostHandler = async () => {
+
+   
+    
+  
+    // Get the value of the "Content" input
+    var content = document.getElementById("text-area").value;
+    var postId=  document.getElementById("text-area").dataset.id;
+  
+    
+    if (content) {
+       const response = await fetch('/api/comments', {
+          method: 'POST',
+          body: JSON.stringify({ post_id:postId, content }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (response.ok) {
+        
+          document.location.reload();
+        } else {
+          alert('Failed to sign up.');       }
+   
+    
+  }
+};
+document.getElementById("comment").addEventListener("click", newPostHandler)
