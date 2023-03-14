175eb50e31e1b1134c3e0f86470c1b3731ccc3bc
Majid-pkz <majid.computing@gmail.com>
2023-03-15 00:42:29 +1100

    add router commentRouites

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const dashboardRoutes = require('./dashboardRoutes')
+const commentRoutes = require('./commentRoutes')
 
 
 
@@ -11,6 +12,7 @@ const dashboardRoutes = require('./dashboardRoutes')
 router.use('/users', userRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 
 module.exports = router;
