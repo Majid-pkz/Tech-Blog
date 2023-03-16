e31edfc5b45b07729e89982db5400dc8a505d01b
Majid-pkz <majid.computing@gmail.com>
2023-03-16 13:07:02 +1100

    make sign-up leading to immediate login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,24 +2,20 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // CREATE new user
+
 router.post('/sign-up', async (req, res) => {
   try {
-    const dbUserData = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-console.log(`user_id when signing-up is ${dbUserData.id}`)
-    // Set up sessions with a 'loggedIn' variable set to `true`
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      // req.session.loggedIn = true;
+    const userData = await User.create(req.body);
 
-      res.status(200).json(dbUserData);
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
     });
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
